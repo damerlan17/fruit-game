7d7aff372d14f2fc098f4cfe4939a9cf28bd7867
damerlan17 <uchunakdamelan@gmail/com>
2025-10-06 11:08:27 +0700

    player moow

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,109 @@
+class Drawable {
+    constructor(game) {
+        this.game = game;
+        this.x = 0;
+        this.y = 0;
+        this.w = 0;
+        this.h = 0;
+        this.offsets = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    update() {
+        this.x += this.offsets.x
+        this.y += this.offsets.y
+    }
+
+    createElement() {
+        this.element = document.createElement("div");
+        this.element.className = "element " + this.constructor.name.toLowerCase();
+        $('.elements').append(this.element);
+    }
+
+    draw() {
+        this.element.style = `
+            left: ${this.x}px;
+            top: ${this.y}px;
+            width: ${this.w}px;
+            height: ${this.h}px;
+        `;
+    }
+}
+
+class Player extends Drawable {
+    constructor(game) {
+        super(game);
+        this.w = 244;
+        this.h = 109;
+        this.x = window.innerWidth / 2 - this.w / 2;
+        this.x = window.innerWidth / 2 - this.w / 2;
+        this.y = window.innerHeight - this.h;
+        this.speadPerFrame =20
+        this.keys = {
+            ArrowLeft: false,
+            ArrowRight: false,
+        }
+        this.createElement();
+        this.bindKeyEvents()
+    }
+
+    bindKeyEvents() {
+        document.addEventListener('keydown', ev => this.changeKeyStatus(ev.code, true))
+        document.addEventListener('keyup', ev => this.changeKeyStatus(ev.code, false))
+    }
+
+    changeKeyStatus(code, value) {
+        if(code in this.keys) this.keys[code] = value;
+    }
+
+    update() {
+        if(this.keys.ArrowLeft && this.x > 0) this.offsets.x = -this.speadPerFrame
+        else if(this.keys.ArrowRight && this.x < window.innerWidth - this.w) this.offsets.x = this.speadPerFrame
+        else this.offsets.x = 0
+        super.update()
+    }
+}
+
 class Game {
     constructor() {
-        this.name = name
+        this.name = name;
+        this.elements = [];
+        this.player = this.generate(Player);
     }
 
-    start (){
-        this.loop()
+    start () {
+        this.loop();
     }
+
+    generate(className) {
+        let element = new className(this);
+        this.elements.push(element);
+        return element;
+    }
+
     loop() {
         requestAnimationFrame(() => {
-            this.setParams()
-            this.loop()
+            this.updateElements();
+            this.setParams();
+            this.loop();
+        });
+    }
+
+    updateElements() {
+        this.elements.forEach(element => {
+            element.update();
+            element.draw();
+
         })
-    }//анимация
+    }
 
     setParams() {
-        let params = ['name']
-        let values = [this.name]
+        let params = ['name'];
+        let values = [this.name];
         params.forEach((param, ind) => {
-            $(`#${param}`).innerHtml = values[ind]
-        })
+            $(`#${param}`).innerHTML = values[ind];
+        });
     }
 }
